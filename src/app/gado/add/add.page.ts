3052cab6c00f8a8f46463140b5a3b5ff3da240ae
Arthur Fritz Santiago <arthurfritzz@gmail.com>
2019-05-11 05:32:36 -0300

    Inclusão de foto

--- src/app/gado/add/add.page.ts
+++ src/app/gado/add/add.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 @Component({
   selector: 'app-add',
@@ -6,10 +10,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add.page.scss'],
 })
 export class AddPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  
+  ngOnInit(): void {
+   
   }
+
+  private addPage : FormGroup;
+  public base64Image;
+  private options: CameraOptions = {
+    quality: 20,
+    destinationType: this._camera.DestinationType.FILE_URI,
+    encodingType: this._camera.EncodingType.JPEG,
+    mediaType: this._camera.MediaType.PICTURE,
+   // allowEdit: true,
+    correctOrientation: true
+  }
+
+  constructor(private _router:Router,  
+    private formBuilder: FormBuilder, 
+    private _camera: Camera,
+    private file: File) {
+
+    this.addPage = this.formBuilder.group({
+      nome: ['', Validators.required],
+      identificacao: ['', Validators.required]
+    });
+    
+   }
+
+   public takePhoto(){
+    this._camera.getPicture(this.options).then((imageData) => {
+      let filename = imageData.substring(imageData.lastIndexOf('/')+1);
+      let path =  imageData.substring(0,imageData.lastIndexOf('/')+1);
+         //then use the method reasDataURL  btw. var_picture is ur image variable
+      this.file.readAsDataURL(path, filename).then(res=> this.base64Image = res  ).catch(err=>{
+        alert("erro2" + err);
+        this.base64Image = null;  
+      });
+     }, (err) => {
+      alert("erro1" + err);
+      this.base64Image = null;
+     });
+   }
+
+
+   public callRegister(){
+    let identifier = "1"
+    this._router.navigateByUrl("/tabs/gado/detail/"+identifier);
+   }
+
 
 }
