e293b47b00b869ded5cb0f51bdaf6ed82da4e7ff
Peterson Vieira Salme <petersonsalme@gmail.com>
2019-05-11 05:45:11 -0300

    Using expandable component

--- src/app/lote/lote.module.ts
+++ src/app/lote/lote.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
+import { ExpandableModule } from "../components/expandable/expandable.module";
 
 import { LotePage } from './lote.page';
 
@@ -19,6 +20,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    ExpandableModule,
     RouterModule.forChild(routes)
   ],
   declarations: [LotePage]
--- src/app/lote/lote.page.ts
+++ src/app/lote/lote.page.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LotePage implements OnInit {
 
+  public items: any = [
+    { expanded: false },
+    { expanded: false },
+    { expanded: false },
+    { expanded: false },
+    { expanded: false },
+    { expanded: false },
+    { expanded: false },
+    { expanded: false },
+    { expanded: false }
+  ];
+  
+    expandItem(item): void {
+      if (item.expanded) {
+        item.expanded = false;
+      } else {
+        this.items.map(listItem => {
+          if (item == listItem) {
+            listItem.expanded = !listItem.expanded;
+          } else {
+            listItem.expanded = false;
+          }
+          return listItem;
+        });
+      }
+    }
+
   constructor() { }
 
   ngOnInit() {
